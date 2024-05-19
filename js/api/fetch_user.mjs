1cdb11419e9d33e9a58d4a1fc1e86919f4596cd5
BergitTveit <134147170+BergitTveit@users.noreply.github.com>
2024-05-19 16:05:55 +0200

    wip

--- js/api/fetch_user.mjs
+++ js/api/fetch_user.mjs
@@ -2,47 +2,53 @@ import { load } from "../storage/load.mjs";
 import { API_BASE, API_SOCIAL } from "./constants.mjs";
 import { headers } from "./headers.mjs";
 
-// export async function fetchUserProfile(name) {
-//   const url = `https://v2.api.noroff.dev/social/profiles/${name}`;
-//   try {
-//     const resolvedHeaders = await headers();
+export async function fetchUserProfile(name) {
+  const url = `https://v2.api.noroff.dev/social/profiles/${name}`;
+  try {
+    const resolvedHeaders = await headers();
+    const response = await fetch(url, {
+      headers: resolvedHeaders,
+    });
 
-//     console.log(resolvedHeaders, "Resolved headers in here...");
+    if (response.ok) {
+      const profileData = await response.json();
+      return profileData;
+    } else {
+      throw new Error("Failed to fetch user profile");
+    }
+  } catch (error) {
+    await alert("Failed to fetch user profile");
+    if (name) {
+      window.location.href = `/index/index.html?name=${name}`;
+    } else {
+      window.location.href = `/index/index.html`;
+    }
+    throw error;
+  }
 
-//     const response = await fetch(url, {
-//       headers: resolvedHeaders,
-//     });
+  //   return response;
+}
 
-//     console.log("name: ", name);
+export function displayUserProfile(userInfo) {
+  const userNameElement = document.getElementById("username");
+  const userEmailElement = document.getElementById("useremail");
 
-//     if (response.ok) {
-//       const profileData = await response.json();
-//       console.log(profileData);
-//       return profileData;
-//     }
-//   } catch {
-//     throw new Error("Failed to fetch user profile");
-//   }
-//   console.log("RESPONSE: ", response);
-//   return response;
-// }
-// fetchUserProfile();
+  userNameElement.textContent = userInfo.name;
+  userEmailElement.textContent = userInfo.email;
+}
 
-// export function displayUserProfile(userInfo) {
-//   const userNameElement = document.getElementById("username");
-//   const userEmailElement = document.getElementById("useremail");
+async function showUserProfile() {
+  try {
+    var profile = load("profile");
+    console.log(profile);
+    const urlParams = new URLSearchParams(window.location.search);
+    const userName = urlParams.get("name");
+    const userInfo = await fetchUserProfile(userName);
+    displayUserInfo(userInfo);
+  } catch (error) {
+    console.error("Error:", error.message);
+    // Handle error, e.g., show error message to the user
+  }
+}
 
-//   userNameElement.textContent = userInfo.name;
-//   userEmailElement.textContent = userInfo.email;
-// }
-
-// async function showUserProfile(name) {
-//   try {
-//     const userInfo = await fetchUserProfile(name);
-//     displayUserInfo(userInfo);
-//   } catch (error) {
-//     console.error("Error:", error.message);
-//     // Handle error, e.g., show error message to the user
-//   }
-// }
-// showUserProfile();
+await showUserProfile();
